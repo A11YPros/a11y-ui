eacff533b8ead16697d03ebf539e600a1dc013f4
ryan0122 <your@email.com>
2026-09-02 10:25:58 -0600

    chore: update sitemap

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -20,6 +20,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${baseUrl}/web-components`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/accessibility`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
